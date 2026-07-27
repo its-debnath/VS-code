a4374a90c3571e3b762133db439e77ac40b26b85
GOURANGA DEBNATH <gourangadebnath860@gmail.com>
2026-07-28 01:15:10 +0530

    normal change

--- python_folder/basic.py
+++ python_folder/basic.py
@@ -128,3 +128,33 @@
 # print(list[-k: ] + list[0:L-k])
 n = 864
 print(str(n)[::-1])
+
+
+
+
+
+# def cal_sum_of_n(n):
+#     if(n==0):
+#         return 0
+#     return n+cal_sum_of_n(n-1)
+# print(cal_sum_of_n(9))
+
+# List = ["Aman","Gouranga","Amit","Kajal","Susmita","Rajanna","Umme"]
+# def print_list(list , idx):
+#     if(idx==len(list)):
+#         return
+#     print(List[idx])
+
+# print(List,5)
+
+# dictt ={
+#     "name" : "amit",
+#     "cgpa" : 9,
+#     "d" : {
+#         "ghar" : "bihar",
+#         "collage" : "iit",
+#         "ghar" : "bihar"
+
+#     }
+
+# }
--- python_folder/git_commands.py
+++ python_folder/git_commands.py
@@ -19,38 +19,3 @@ git commit -m "Added Git command notes"
 git push
 
 '''
-
-
-
-# def cal_sum_of_n(n):
-#     if(n==0):
-#         return 0
-#     return n+cal_sum_of_n(n-1)
-# print(cal_sum_of_n(9))
-
-# List = ["Aman","Gouranga","Amit","Kajal","Susmita","Rajanna","Umme"]
-# def print_list(list , idx):
-#     if(idx==len(list)):
-#         return
-#     print(List[idx])
-
-# print(List,5)
-
-# dictt ={
-#     "name" : "amit",
-#     "cgpa" : 9,
-#     "d" : {
-#         "ghar" : "bihar",
-#         "collage" : "iit",
-#         "ghar" : "bihar"
-
-#     }
-
-# }
-
-
-
-
-name = "amIt"
-print(name.capitalize())
-print(name)
