fbcea83d9bf8a82ed3b3a85f390676062cba0bbc
GOURANGA DEBNATH <gourangadebnath860@gmail.com>
2026-07-28 01:00:59 +0530

    add github command

--- python_folder/hh.py
+++ python_folder/hh.py
@@ -1,7 +1,7 @@
 '''
 -------------------Now that GitHub is working, you can learn the most important Git commands----------------
 
-Command	Purpose
+#Command	Purpose
 git status     	Check what changed
 git add .	    Stage changes
 git commit -m   "message"	Save a version
@@ -9,6 +9,15 @@ git push	    Upload changes to GitHub
 git pull	    Download the latest changes
 git log	        View commit history
                      ---------------------These six commands cover most day-to-day Git usage.
+
+
+#After Edit a file and need to upload on github , this one by one in vs terminal
+
+git status
+git add .
+git commit -m "Added Git command notes"
+git push
+
 '''
 
 
